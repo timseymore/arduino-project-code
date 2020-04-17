224c151c9404c1d9f036ba79c2d8c55d9d64803c
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-16 20:31:23 -0400

    Update RoboCar.ino

--- RoboCar/RoboCar.ino
+++ RoboCar/RoboCar.ino
@@ -1,3 +1,9 @@
+/*
+ *  RoboCar.ino
+ * 
+ *  Object avoiding robot car using arduino uno platform
+*/
+
 #include "SR04.h"
 
 
@@ -28,44 +34,43 @@
 // Motors
 
 // Side A
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
+#define EN_A 9
+#define IN1 8
+#define IN2 7
 
 // Side B
-int enB = 3;
-int in3 = 5;
-int in4 = 4;
+#define EN_B 3
+#define IN3 5
+#define IN4 4
 
 // Distance variables
 long front_distance;
 long right_distance;
 long left_distance;
 
-// Create SR405 objects
+// Create SR04 objects
 SR04 front_sensor = SR04(ECHO_PIN_CENTER, TRIG_PIN_CENTER);
 SR04 right_sensor = SR04(ECHO_PIN_RIGHT, TRIG_PIN_RIGHT);
 SR04 left_sensor = SR04(ECHO_PIN_LEFT, TRIG_PIN_LEFT);
 
 void setup() {
 
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(EN_A, OUTPUT);
+  pinMode(EN_B, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
   pinMode(RIGHT_LED, OUTPUT);
   pinMode(LEFT_LED, OUTPUT);
-
   digitalWrite(RIGHT_LED, LOW);
   digitalWrite(LEFT_LED, LOW);
   
   // blink leds 10 times to indicate start up
   for (int i = 0; i < 10; i++) {
-    blink_leds(RIGHT_LED, LEFT_LED, 250);
-  }
-  
+    blink_leds(RIGHT_LED, LEFT_LED, 200);
+  }  
+
 }
 
 void loop() {
@@ -78,83 +83,87 @@ void loop() {
 }
 
 
-// Functions
+// Helper Functions
 
-// EFFECTS: Moves robot foward at input speed
+// EFFECTS: Moves robot foward at given speed
 void moveFoward(int spd) {
-  // Motor A
-  analogWrite(enA, spd);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  // Motor B
-  analogWrite(enB, spd);
-  digitalWrite(in3, LOW); 
-  digitalWrite(in4, HIGH);
+  // Motor Side A Foward
+  analogWrite(EN_A, spd);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);  
+  // Motor Side B Foward
+  analogWrite(EN_B, spd);
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, HIGH);
 }
 
-// EFFECTS: Moves robot in reverse at input speed
+// EFFECTS: Moves robot reverse at given speed
 void moveReverse(int spd) {
-  // Motor A
-  analogWrite(enA, spd);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  // Motor B
-  analogWrite(enB, spd);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+  // Motor Side A Reverse
+  analogWrite(EN_A, spd);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  // Motor Side B Reverse
+  analogWrite(EN_B, spd);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 // EFFECTS: Turns robot left at input speed
 void turnLeft(int spd) {
-  // Motor A
-  analogWrite(enA, spd);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);  
-  // Motor B
-  analogWrite(enB, spd);
-  digitalWrite(in3, LOW); 
-  digitalWrite(in4, HIGH);
+  // Motor Side A Reverse
+  analogWrite(EN_A, spd);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);  
+  // Motor Side B Foward
+  analogWrite(EN_B, spd);
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, HIGH);
 }
 
 // EFFECTS: Turns robot right at input speed
 void turnRight(int spd) {
-  // Motor A
-  analogWrite(enA, spd);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  // Motor B
-  analogWrite(enB, spd);
-  digitalWrite(in3, HIGH); 
-  digitalWrite(in4, LOW);
+  // Motor Side A Foward
+  analogWrite(EN_A, spd);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);  
+  // Motor Side B Reverse
+  analogWrite(EN_B, spd);
+  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN4, LOW);
 }
 
 // EFFECTS: Stops robot 
 void stop() {
-  // Motor A
-  analogWrite(enA, 0);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  // Motor B
-  analogWrite(enB, 0);
-  digitalWrite(in3, LOW); 
-  digitalWrite(in4, LOW);
+  // Motor Side A Stop
+  analogWrite(EN_A, 0);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);  
+  // Motor Side B Stop
+  analogWrite(EN_B, 0);
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, LOW);
+}
+
+// RETURNS: true if distance is in danger zone
+bool in_danger(int distance) {
+  return distance < MIN;
+}
+
+// RETURNS: true if distance is in safe zone
+bool in_safe(int distance) {
+  return distance > MAX;
+}
+
+// RETURNS: true if distance is in caution zone
+bool in_caution(int distance) {
+  return !in_danger(distance) && !in_safe(distance);
 }
 
 // EFFECTS: set direction based on distance to object
 void handle_object_distance(int front, int right, int left) {
-
-  bool danger_front = front < MIN;
-  bool caution_front = front <= MAX && front >= MIN;
-  bool safe_front = front > MAX;
-  bool danger_right = right < MIN;
-  bool caution_right = right <= MAX && right >= MIN;
-  bool safe_right = right > MAX;
-  bool danger_left = left < MIN;
-  bool caution_left = left <= MAX && left >= MIN;
-  bool safe_left = left > MAX;
-
   // Danger Any
-  if (danger_front || danger_right || danger_left) {
+  if (in_danger(front) || in_danger(right) || in_danger(left)) {
     // stop, debug LEDs on,  and back up double speed
     stop();
     digitalWrite(RIGHT_LED, HIGH);
@@ -162,21 +171,21 @@ void handle_object_distance(int front, int right, int left) {
     moveReverse(DOUBLE_SPEED);
   }
   // Safe All
-  else if (safe_front && safe_right && safe_left) {
+  else if (in_safe(front) && in_safe(right) && in_safe(left)) {
     // debug LEDs off and full speed ahead
     digitalWrite(RIGHT_LED, LOW);
     digitalWrite(LEFT_LED, LOW);
     moveFoward(SPEED);
   }
   // Caution All
-  else if (caution_front && caution_right && caution_left) {
+  else if (in_caution(front) && in_caution(right) && in_caution(left)) {
     // debug LEDs on and back up normal speed
     digitalWrite(RIGHT_LED, HIGH);
     digitalWrite(LEFT_LED, HIGH);
     moveReverse(SPEED);
   }
   // Caution Front, Right Safe, Left Safe
-  else if (caution_front && safe_right && safe_left) {
+  else if (in_caution(front) && in_safe(right) && in_safe(left)) {
     // stop and pick longest safe path of left or right
     stop();
     if (left > right) {
@@ -187,21 +196,21 @@ void handle_object_distance(int front, int right, int left) {
     }
   }
   // Safe Front, Right Caution, Left Caution
-  else if (safe_front && caution_left && caution_right) {
+  else if (in_safe(front) && in_caution(left) && in_caution(right)) {
     // debug lights on and back up normal speed
     digitalWrite(RIGHT_LED, HIGH);
     digitalWrite(LEFT_LED, HIGH);
     moveReverse(SPEED);
   }
   // Any Front, Right Caution, Left Safe
-  else if (caution_right && safe_left) {
+  else if (in_caution(right) && in_safe(left)) {
     // turn on right LED and turn left
     digitalWrite(RIGHT_LED, HIGH);
     digitalWrite(LEFT_LED, LOW);
     turnLeft(DOUBLE_SPEED);
   }
   // Any Front, Right Safe, Left Caution
-  else if (caution_left && safe_right) {
+  else if (in_caution(left) && in_safe(right)) {
     // turn on left LED and turn right
     digitalWrite(RIGHT_LED, LOW);
     digitalWrite(LEFT_LED, HIGH);
