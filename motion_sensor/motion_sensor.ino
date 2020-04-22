654f74815bc958e2343ad06b4ac33a86071b9c2a
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-21 22:02:23 -0400

    Update motion_sensor.ino

--- motion_sensor/motion_sensor.ino
+++ motion_sensor/motion_sensor.ino
@@ -1,69 +1,66 @@
 // Motion Sensor
 
 // Define constants
-#define wait_led  11
-#define ready_led  12
-#define triggered_led 13
-#define motion_trigger 7 
+#define WAIT_LED  11
+#define READY_LED  12
+#define TRIGGERED_LED 13
+#define MOTION_TRIGGER 7 
 
 // Define Variables
 int motion_detected = 0;
 int motion_value;
 
 
-
 void setup() {
   Serial.begin(9600);
-  pinMode(wait_led, OUTPUT);
-  pinMode(ready_led, OUTPUT);
-  pinMode(triggered_led, OUTPUT);
-  pinMode(motion_trigger, INPUT);
+  pinMode(WAIT_LED, OUTPUT);
+  pinMode(READY_LED, OUTPUT);
+  pinMode(TRIGGERED_LED, OUTPUT);
+  pinMode(MOTION_TRIGGER, INPUT);
 
   // Inital 1 minute delay to stabalize sensor
-  digitalWrite(triggered_led, LOW);
-  digitalWrite(ready_led, LOW);
-  digitalWrite(wait_led, HIGH);
+  digitalWrite(TRIGGERED_LED, LOW);
+  digitalWrite(READY_LED, LOW);
+  digitalWrite(WAIT_LED, HIGH);
   delay(60000);
-  digitalWrite(ready_led, HIGH);
-  digitalWrite(wait_led, LOW);  
+  digitalWrite(READY_LED, HIGH);
+  digitalWrite(WAIT_LED, LOW);  
 
 }
 
 void loop() 
 {
   // Get value from motion sensor
-  motion_value = digitalRead(motion_trigger);
+  motion_value = digitalRead(MOTION_TRIGGER);
 
   // See if motion detected
   if (motion_value == 1)
   {
     // Display triggered led for 3 secs
-    digitalWrite(triggered_led, HIGH);
+    digitalWrite(TRIGGERED_LED, HIGH);
     motion_detected = 1;
     delay(3000);
   }
     
   else
   {
-    digitalWrite(triggered_led, LOW);
+    digitalWrite(TRIGGERED_LED, LOW);
   }
 
   // Add delay after triggering to reset sensor
   if (motion_detected == 1)    
   {
-    Serial.print("Motion Detected!);
+    Serial.print("Motion Detected!");
     
     // After trigger, wait 6 sec to re-arm
-    digitalWrite(triggered_led, LOW);
-    digitalWrite(ready_led, LOW);
-    digitalWrite(wait_led, HIGH);
+    digitalWrite(TRIGGERED_LED, LOW);
+    digitalWrite(READY_LED, LOW);
+    digitalWrite(WAIT_LED, HIGH);
     delay(6000);
-    digitalWrite(ready_led, HIGH);
-    digitalWrite(wait_led, LOW);
+    digitalWrite(READY_LED, HIGH);
+    digitalWrite(WAIT_LED, LOW);
     motion_detected = 0;
   }
 
   
 }
-
-
