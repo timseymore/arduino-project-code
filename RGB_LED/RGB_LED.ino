f6e2eb100bcb7901de36972a907617bcd9235037
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-22 22:51:49 -0400

    Update RGB_LED.ino

--- RGB_LED/RGB_LED.ino
+++ RGB_LED/RGB_LED.ino
@@ -6,78 +6,58 @@
 #define GREEN 5
 #define RED 3
 
-void setup()
-{
-pinMode(RED, OUTPUT);
-pinMode(GREEN, OUTPUT);
-pinMode(BLUE, OUTPUT);
-digitalWrite(RED, HIGH);
-digitalWrite(GREEN, LOW);
-digitalWrite(BLUE, LOW);
-}
+// fading time between colors
+#define DELAY_TIME 10 
 
 // define variables
 int redValue;
 int greenValue;
 int blueValue;
 
-// main loop
-void loop()
-{
-#define delayTime 10 // fading time between colors
 
-redValue = 255; // choose a value between 1 and 255 to change the color.
-greenValue = 0;
-blueValue = 0;
-
-// this is unnecessary as we've either turned on RED in SETUP
-// or in the previous loop ... regardless, this turns RED off
-// analogWrite(RED, 0);
-// delay(1000);
-
-for(int i = 0; i < 255; i += 1) // fades out red bring green full when i=255
-{
-redValue -= 1;
-greenValue += 1;
-// The following was reversed, counting in the wrong directions
-// analogWrite(RED, 255 - redValue);
-// analogWrite(GREEN, 255 - greenValue);
-analogWrite(RED, redValue);
-analogWrite(GREEN, greenValue);
-delay(delayTime);
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
 }
 
-redValue = 0;
-greenValue = 255;
-blueValue = 0;
+void loop() {    
 
-for(int i = 0; i < 255; i += 1) // fades out green bring blue full when i=255
-{
-greenValue -= 1;
-blueValue += 1;
-// The following was reversed, counting in the wrong directions
-// analogWrite(GREEN, 255 - greenValue);
-// analogWrite(BLUE, 255 - blueValue);
-analogWrite(GREEN, greenValue);
-analogWrite(BLUE, blueValue);
-delay(delayTime);
+    redValue = 255; 
+    greenValue = 0;
+    blueValue = 0;
+
+    // fade out red bring green full when i=255
+    fade (redValue, greenValue, RED, GREEN);
+
+    redValue = 0;
+    greenValue = 255;
+    blueValue = 0;
+
+    // fade out green bring blue full when i=255
+    fade (greenValue, blueValue, GREEN, BLUE);
+
+    redValue = 0;
+    greenValue = 0;
+    blueValue = 255;
+
+    // fade out blue bring red full when i=255
+    fade (blueValue, redValue, BLUE, RED);
+
 }
 
-redValue = 0;
-greenValue = 0;
-blueValue = 255;
 
-for(int i = 0; i < 255; i += 1) // fades out blue bring red full when i=255
-{
-// The following code has been rearranged to match the other two similar sections
-blueValue -= 1;
-redValue += 1;
-// The following was reversed, counting in the wrong directions
-// analogWrite(BLUE, 255 - blueValue);
-// analogWrite(RED, 255 - redValue);
-analogWrite(BLUE, blueValue);
-analogWrite(RED, redValue);
-delay(delayTime);
-}
+void fade (int out_val, int in_val, int out_pin, int in_pin) {
+    for(int i = 0; i < 255; i += 1) {
+        out_val -= 1;
+        in_val += 1;
+        analogWrite(out_pin, out_val);
+        analogWrite(in_pin, in_val);
+        delay(DELAY_TIME);
+    }
+
 }
 
