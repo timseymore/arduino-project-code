6b2fc2c393b07c58d786a284a3a05b70f7388ff4
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-23 20:11:59 -0400

    Update RGB_LED.ino

--- RGB_LED/RGB_LED.ino
+++ RGB_LED/RGB_LED.ino
@@ -1,50 +1,49 @@
-//www.elegoo.com
-//2016.12.8
+/*
+    RGB LED Fader
+*/
 
-// Define Pins
+
+// LED Pins
 #define BLUE 6
 #define GREEN 5
 #define RED 3
 
-// fading time between colors
 #define DELAY_TIME 10 
-
-// define variables
-int redValue;
-int greenValue;
-int blueValue;
-
 
 void setup () {
     pinMode(RED, OUTPUT);
     pinMode(GREEN, OUTPUT);
     pinMode(BLUE, OUTPUT);
-    digitalWrite(RED, HIGH);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
 }
 
 void loop () {    
 
-    one_full (redValue. greenValue, blueValue);
+    // start loop with full red
+    one_full (RED, GREEN, BLUE);
 
-    // fade out red bring green full when i=255
-    fade (redValue, greenValue, RED, GREEN);
+    // fade red to green
+    fade (RED, GREEN);
 
-    one_full (greenValue, redValue, blueValue);
+    // ensure full green only
+    one_full (GREEN, RED, BLUE);
 
-    // fade out green bring blue full when i=255
-    fade (greenValue, blueValue, GREEN, BLUE);
+    // fade green to blue
+    fade (GREEN, BLUE);
 
-    one_full (blueValue, redValue, greenValue);
+    // ensure blue full only
+    one_full (BLUE, RED, GREEN);
 
-    // fade out blue bring red full when i=255
-    fade (blueValue, redValue, BLUE, RED);
+    // fade blue to red
+    fade (BLUE, RED);
 
 }
 
-
-void fade (int out_val, int in_val, int out_pin, int in_pin) {
+// Fade out_val from full to off while bringing in_val to full from off
+// - out_pin : pin number for led to fade out
+// - in_pin : pin number for led to fade in
+void fade (int out_pin, int in_pin) {
+    int out_val = 255;
+    int in_val = 0;
     for(int i = 0; i < 255; i += 1) {
         out_val -= 1;
         in_val += 1;
@@ -52,10 +51,16 @@ void fade (int out_val, int in_val, int out_pin, int in_pin) {
         analogWrite(in_pin, in_val);
         delay(DELAY_TIME);
     }
+    // ensure correct brightness when exiting 
+    analogWrite(out_pin, 0);
+    analogWrite(in_pin, 255);
 
 }
 
-
+// Turn on one led full while other two are turned off
+// - on : pin number for led to turn on
+// - off1 : pin number for led to turn off
+// - off2 : pin number for led to turn off
 void one_full (int on, int off1, int off2) {
     analogWrite(on, 255);
     analogWrite(off1, 0);
