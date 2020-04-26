887ce822de54f96b1c6627dd923159cc9aae7392
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-26 11:44:01 -0400

    Update MyBlink.ino

--- MyBlink/MyBlink.ino
+++ MyBlink/MyBlink.ino
@@ -3,16 +3,15 @@
 
 */
 
-const int delay_time = 500;
+const int DELAY_TIME = 500;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-
 void loop() { 
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(delay_time);                    
+  delay(DELAY_TIME);                    
   digitalWrite(LED_BUILTIN, LOW);
-  delay(delay_time);                    
+  delay(DELAY_TIME);                    
 }
