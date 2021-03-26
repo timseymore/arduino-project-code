336a914e6b2b4be7e92b037ac740323f0bd4fad0
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-26 04:42:24 -0400

    refactor: Update motion_range_finder.ino
    Refactor the script to keep it clean and managable.

--- motion_range_finder/motion_range_finder.ino
+++ motion_range_finder/motion_range_finder.ino
@@ -1,150 +1,162 @@
 /* 
  * Motion Sensor and Range Finder with LCD Display 
+ * 
+ * 2019 Tim Seymore
  */
 
 #include <LiquidCrystal.h>
 #include <SR04.h>
 
 
-// Define pins for LCD display
-const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+///////////////
+// Constants //
+///////////////
 
-// Define sensor pins
-#define TRIG_PIN 10
-#define ECHO_PIN 9
-#define PIR_PIN 8
-#define DETECTED_LED 11
-#define READY_LED 12
-#define WAIT_LED 13
+#define TRUE  0
+#define FALSE 1
+#define PIR_TRIG_PIN  8
+#define SR_ECHO_PIN   9
+#define SR_TRIG_PIN  10
+#define DETECT_LED   11
+#define READY_LED    12
+#define WAIT_LED     13
 
-// TODO: Implement OOP
-class System {
-  private:
-    LiquidCrystal lcd;
-    SR04 sr04;
-    int pirValue;
-    int motionDetected;
-    float distance;
-    int timesDetected;
-    float shortestDistance;
-    float lastDistance;
+
+/////////////
+// Classes //
+/////////////
+
+class SystemManager {
 
   public:
-    System (LiquidCrystal& lcd, SR04& sr04)
-    :lcd(lcd)
-    ,sr04(sr04)
+    SystemManager ()
     {
-      init();
+      this->init();
     };
 
     void init () {
-      motionDetected = 0; // 0 for false, 1 for true
-      timesDetected = 0;
-      shortestDistance = 0; // set for a default
-
-      pinMode(DETECTED_LED, OUTPUT);
-      pinMode(READY_LED, OUTPUT);
-      pinMode(WAIT_LED, OUTPUT);
-      pinMode(PIR_PIN, INPUT);
-
-      digitalWrite(DETECTED_LED, LOW);
-      digitalWrite(READY_LED, LOW);
-      digitalWrite(WAIT_LED, HIGH); 
-
-      lcd.begin(16, 2);
-      printWarmupMessage();
+      Serial.begin(9600);
+      Serial.print("Setting up System Manager...");
     }
 
-  void run() {
-      // Get sensor data
-      pirValue = digitalRead(PIR_PIN);
-      distance = sr04.distance;
-
-      // check for and handle motion events
-      if (pirValue == 1) {    
-        
-        lastDistance = distance;
-        if (shortestDistance == 0 || distance < shortestDistance) {
-          shortestDistance = distance;
-        } 
-        handleMotionDetected(distance);    
-        motionDetected = 1;
-      }
-
-      if (motionDetected == 1) {
-        rearmSystem();
-        timesDetected++;
-        motionDetected = 0;
-      }
-
-      // Return to ready state
-      systemArmedState();
+    void run() {
+      Serial.print("Running System Manager...");
     }
 
-    void printWarmupMessage() {      
-      lcd.setCursor(2, 0); // center the text, 2 spaces from the left edge
-      lcd.print("Motion Finder");
-      delay(10000);
-      lcd.clear();
-
-      int count = 0;
-      for (int i = 0; i < 10; i++) {
-        lcd.setCursor(0, 0);
-        lcd.print("Warming up");
-        lcd.setCursor(10, 0);
-        lcd.print(getDots(count));
-        delay(5000);
-        lcd.clear();
-        count++;
-        // keep count between 0 and 3 inclusive
-        if (count > 3) {
-          count = 0;
-        }        
-      }
-    }
-    
-};
+}; 
 
 
-SR04 sr04(ECHO_PIN, TRIG_PIN);
-System system(lcd, sr04);
+//////////////////////
+// Global Variables //
+//////////////////////
 
-// Define variables
-int pirValue;
-int motionDetected;
-float distance;
-int timesDetected;
-float shortestDistance;
-float lastDistance;
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+SR04          sr04(SR_ECHO_PIN, SR_TRIG_PIN);
+SystemManager system();
+
+int   pirValue;
+int   motionDetected;
+int   timesDetected;
+long  distance;
+long  shortestDistance;
+long  lastDistance;
 
 
 ///////////////////
 // Setup And Run //
 ///////////////////
 
-void setup() {}
+void setup() {
+  
+  pinMode(DETECT_LED,   OUTPUT);
+  pinMode(READY_LED,    OUTPUT);
+  pinMode(WAIT_LED,     OUTPUT);
+  pinMode(PIR_TRIG_PIN, INPUT);
 
-void loop() { system.run(); }
+  digitalWrite(DETECT_LED, LOW);
+  digitalWrite(READY_LED,  LOW);
+  digitalWrite(WAIT_LED,   HIGH);
+  
+  motionDetected   = FALSE; 
+  timesDetected    = 0;
+  shortestDistance = 0; // set for a default 
+
+  lcd.begin(16, 2);
+
+  printWarmupMessage();
+
+}
+
+void loop() { 
+
+    // Get sensor data
+  pirValue = digitalRead(PIR_TRIG_PIN);
+  distance = sr04.Distance();
+
+  // Local variables
+  bool isMovement   = motionDetected == TRUE;      
+  bool pirTriggered = pirValue == 1;
+  bool isShortest   = shortestDistance == 0 || distance < shortestDistance;
+
+  // Check for and handle motion events
+  if (pirTriggered) {        
+    lastDistance = distance;
+    if (isShortest) {
+      shortestDistance = distance;
+    } 
+    handleMotionDetected(distance);    
+    motionDetected = TRUE;
+  }
+
+  // Rearm after motion detected
+  if (isMovement) {
+    rearmSystem();
+    timesDetected++;
+    motionDetected = FALSE;
+  }
+
+  // Return to ready state
+  systemArmedState();
+  
+}
 
 
 //////////////////////
 // Helper Functions //
 //////////////////////
 
+void printWarmupMessage() {      
+  lcd.setCursor(2, 0); // center the text, 2 spaces from the left edge
+  lcd.print("Motion Finder");
+  delay(10000);
+  lcd.clear();
 
-// TODO: refactor all helpers into System class
+  int count = 0;
+  for (int i = 0; i < 10; i++) {
+    lcd.setCursor(0, 0);
+    lcd.print("Warming up");
+    lcd.setCursor(10, 0);
+    lcd.print(getDots(count));
+    delay(5000);
+    lcd.clear();
+    count++;
+    // keep count between 0 and 3 inclusive
+    if (count > 3) {
+      count = 0;
+    }        
+  }
+}
 
-// EFFECTS: returns string of dots according to value of c
-String getDots(int c) {
+String getDots(int i) {
   String dots;
-  if (c == 0) {
+  if (i == 0) {
     dots = "";
   }
-  else if (c % 2 == 0) {
+  else if (i % 2 == 0) {
     dots = "..";
   }
-  else if (c % 3 == 0) {
+  else if (i % 3 == 0) {
     dots = "...";
   }
   else {
@@ -153,8 +165,6 @@ String getDots(int c) {
   return dots;
 }
 
-// EFFECTS: Prints alert message to display, with distance from triggered event
-//          displays triggered LED 
 void handleMotionDetected(float current) {
   // set the cursor to column 0, line 1
   lcd.setCursor(0, 0);
@@ -172,30 +182,14 @@ void handleMotionDetected(float current) {
   }    
   // Display Triggered LED
   digitalWrite(READY_LED, LOW);
-  digitalWrite(DETECTED_LED, HIGH);
+  digitalWrite(DETECT_LED, HIGH);
   // Display triggered state for 3 seconds
   delay(3000);
 }
 
-// // EFFECTS: returns the calculated distance in cm from HC-SR04 sensor
-// float calculateDistance() {
-//   // Write a pulse to the HC-SR04 Trigger Pin  
-//   digitalWrite(TRIG_PIN, LOW);
-//   delayMicroseconds(2);
-//   digitalWrite(TRIG_PIN, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(TRIG_PIN, LOW);  
-//   // Measure the response from the HC-SR04 Echo Pin 
-//   float duration = pulseIn(ECHO_PIN, HIGH);  
-//   // Determine distance from duration
-//   // Using 343 metres per second as speed of sound  
-//   return (duration / 2) * 0.0343;
-// }
-
-// EFFECTS: displays wait led and prints rearm message to screen for 6 seconds
 void rearmSystem() {
   // Reset LEDs to wait position      
-  digitalWrite(DETECTED_LED, LOW);
+  digitalWrite(DETECT_LED, LOW);
   digitalWrite(READY_LED, LOW);
   digitalWrite(WAIT_LED, HIGH);
   // Print message to display
