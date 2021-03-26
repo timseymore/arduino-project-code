24c27c0260082a80f382f5e9e920fb0e45cdbc3a
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-26 04:40:43 -0400

    refactor: Update SR04_Test.ino
    Refactor the script to keep it clean and managable.

--- SR04_Test/SR04_Test.ino
+++ SR04_Test/SR04_Test.ino
@@ -1,58 +1,63 @@
-/* SR04 Test Code */
+/*  SR04 Test Code 
+  
+    Use with LEDs to debug a SR04 Distance sensor in the Serial Monitor.
+
+*/
 
 #include "SR04.h"
 
 
-#define TRIG_PIN 10
 #define ECHO_PIN 9
+#define TRIG_PIN 10
 
-#define RED 13
-#define YELLOW 12
-#define GREEN 11 
+#define RED   13
+#define GREEN 11
+#define BLUE  12
 
-#define MAX 20
-#define MIN 10
-
+#define CAUTION_BOUNDS 20
+#define DANGER_BOUNDS 10
 
 long distance;
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
 
 void setup() { 
   Serial.begin(9600);
   digitalWrite(RED, LOW);
-  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, LOW);
   digitalWrite(GREEN, LOW);
 }
 
 
 void loop() {
-  Serial.println(distance);
   distance = sr04.Distance();
-  handle_led(distance);   
+  handle_led(distance);
+  Serial.print("Distance: ");    
+  Serial.print(distance);
+  Serial.println("cm");  
 }
 
 
-// EFFECTS: switch on appropriate LED for given distance
-//          GREEN  - safe
-//          YELLOW - caution
-//          RED    - danger
+// Switch on appropriate LED for given distance
+//  - GREEN -> Safe
+//  - BLUE  -> Caution
+//  - RED   -> Danger
 void handle_led(int dist) {
   // Danger
-  if (dist < MIN) {
+  if (dist < DANGER_BOUNDS) {
     digitalWrite(RED, HIGH);
-    digitalWrite(YELLOW, LOW);
+    digitalWrite(BLUE, LOW);
     digitalWrite(GREEN, LOW);
   }
   // Safe
-  else if (dist > MAX) {
+  else if (dist > CAUTION_BOUNDS) {
     digitalWrite(RED, LOW);
-    digitalWrite(YELLOW, LOW);
+    digitalWrite(BLUE, LOW);
     digitalWrite(GREEN, HIGH);
   }
   // Caution
   else {
     digitalWrite(RED, LOW);
-    digitalWrite(YELLOW, HIGH);
+    digitalWrite(BLUE, HIGH);
     digitalWrite(GREEN, LOW);
   }
 }
