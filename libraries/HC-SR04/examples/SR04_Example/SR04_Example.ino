123946f2a7de29a7475ca50a638d068108fd676b
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-26 04:44:10 -0400

    feat: Add LiguiCrystal library
    Add the LiguidCrystal library to easily use LCD displays.

--- libraries/HC-SR04/examples/SR04_Example/SR04_Example.ino
+++ libraries/HC-SR04/examples/SR04_Example/SR04_Example.ino
@@ -1,17 +1,27 @@
+/*  SR04 Example Usage  
+
+    A refactored version of the Arduino example sketch.
+
+ */
+
 #include "SR04.h"
-#define TRIG_PIN 12
-#define ECHO_PIN 11
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
+
+
+#define ECHO_PIN   11
+#define TRIG_PIN   12
+#define DELAY_TIME 1000
+
+long distance;
+SR04 sr04 = SR04(SR_ECHO_PIN, SR_TRIG_PIN);
 
 void setup() {
    Serial.begin(9600);
-   delay(1000);
 }
 
 void loop() {
-   a=sr04.Distance();
-   Serial.print(a);
+   distance = sr04.Distance();
+   Serial.print("Distance: ");
+   Serial.print(distance);
    Serial.println("cm");
-   delay(1000);
+   delay(DELAY_TIME);
 }
